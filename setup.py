e6fe85abdf453b9798c084e458e241ff88db42cd
Michael <mihail.tchernov@yandex.ru>
2018-02-20 10:53:55 +0300

    version 0.0.3 условно протестированы функции расчета корреляции и выборка коэф-тов корреляции в ГРП. Не протестирована функция вычисления временной задержки в отсчетах

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='SeisCore',
-      version='0.0.2',
+      version='0.0.3',
       packages=find_packages(),
       description='Package for processing of microseismic data',
       author='Michael Chernov',
       author_email='mikkoartic@gmail.com',
       license='MIT',
-      zip=False, install_requires=['numpy', 'scipy', 'pywavelets'])
+      zip=False,
+      install_requires=['numpy', 'scipy', 'pywavelets','matplotlib'])
