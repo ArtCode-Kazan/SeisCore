724affaff26ef356caf0d06ce93c3373f614dbbf
Michael <mihail.tchernov@yandex.ru>
2018-03-09 22:42:30 +0300

    Удален экспериментальный модуль на Cython CorrelationCalculation

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup
 from setuptools import find_packages
+from setuptools.dist import Distribution
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
+
 
 setup(name='SeisCore',
-      version='0.0.10',
+      version='0.0.11',
       packages=find_packages(),
       description='Package for processing of microseismic data',
       author='Michael Chernov',
       author_email='mikkoartic@gmail.com',
       license='MIT',
       zip=False,
+      include_package_data=True,
+      distclass=BinaryDistribution,
+      package_data={'HydroFracCore/CalcFunctions/CLibrary': [
+       'CorrelationCalculation.cpython-36m-i386-linux-gnu.so']},
       install_requires=['numpy', 'scipy', 'pywavelets', 'matplotlib',
                         'Cython'])
