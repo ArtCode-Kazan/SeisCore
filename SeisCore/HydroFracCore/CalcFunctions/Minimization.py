e6f5b0b9f80ccb444e50bf5d4278fb17fb17abcf
Michael <mihail.tchernov@yandex.ru>
2018-04-02 16:19:02 +0300

    version 0.0.26 Предварительно завершена работа над функцией минимизации для калибровки скоростей. ТЕСТИРОВАНИЕ ЕЩЕ НЕ СДЕЛАНО.

--- SeisCore/HydroFracCore/CalcFunctions/Minimization.py
+++ SeisCore/HydroFracCore/CalcFunctions/Minimization.py
@@ -1,5 +1,4 @@
 from scipy.optimize import minimize
-import numpy as np
 
 
 def target_function_coordinates(params, args):
@@ -32,8 +31,70 @@ def target_function_coordinates(params, args):
         # корреляция
         correlation = current_data[7]
 
-        t_1 = ((x-x_a)**2+(y-y_a)**2+z**2)**0.5/velocity_a
-        t_2 = ((x-x_b)**2+(y-y_b)**2+z**2)**0.5/velocity_b
+        t_1 = ((x - x_a) ** 2 + (y - y_a) ** 2 + z ** 2) ** 0.5 / velocity_a
+        t_2 = ((x - x_b) ** 2 + (y - y_b) ** 2 + z ** 2) ** 0.5 / velocity_b
+
+        result += (((t_2-t_1)*frequency-delta_moments)*correlation)**2
+    return result
+
+
+def target_function_velocities(params, args):
+    """
+    Целевая функция минимизации для поиска скоростей
+    :param params: неизвестные координаты события - x, y, z, v1,v2,v3,...,vn
+    :param args: известные параметры функции массив и частота дискретизации
+    :return:
+    """
+    # координаты, которые требуется найти
+    x=params[0]
+    y=params[1]
+    z=params[2]
+    velocities=params[3:]
+
+    # параметры-постоянные для построения функции (массив и частота)
+    points_list, coeffs, frequency = args
+    # сама функция
+    result = 0
+    # обход по всем константам (слагаемым)
+    for point_a_number, x_a, y_a, point_b_number, x_b, y_b, delta_moments, \
+        correlation in coeffs:
+        # скорость первой точки
+        velocity_a = velocities[points_list.index(point_a_number)]
+        # скорость второй точки
+        velocity_b = velocities[points_list.index(point_b_number)]
+
+        t_1 = ((x - x_a) ** 2 + (y - y_a) ** 2 + z ** 2) ** 0.5 / velocity_a
+        t_2 = ((x - x_b) ** 2 + (y - y_b) ** 2 + z ** 2) ** 0.5 / velocity_b
+
+        result+=(((t_2-t_1)*frequency-delta_moments)*correlation)**2
+    return result
+
+
+
+def target_function_velocities(params, args):
+    """
+    Целевая функция минимизации для поиска скоростей
+    :param params: неизвестные координаты события - x, y, z, v1,v2,v3,...,vn
+    :param args: известные параметры функции массив и частота дискретизации
+    :return:
+    """
+    # координаты, которые требуется найти
+    x, y, z, velocities = params
+
+    # параметры-постоянные для построения функции (массив и частота)
+    points_list, coeffs, frequency = args
+    # сама функция
+    result = 0
+    # обход по всем константам (слагаемым)
+    for point_a_number, x_a, y_a, point_b_number, x_b, y_b, delta_moments, \
+        correlation in coeffs:
+        # скорость первой точки
+        velocity_a = velocities[points_list.index(point_a_number)]
+        # скорость второй точки
+        velocity_b = velocities[points_list.index(point_b_number)]
+
+        t_1 = ((x - x_a) ** 2 + (y - y_a) ** 2 + z ** 2) ** 0.5 / velocity_a
+        t_2 = ((x - x_b) ** 2 + (y - y_b) ** 2 + z ** 2) ** 0.5 / velocity_b
 
         result += (((t_2-t_1)*frequency-delta_moments)*correlation)**2
     return result
@@ -50,8 +111,30 @@ def minimization_coordinates(coeffs, start_coords, frequency):
     """
     x0, y0, z0 = start_coords
     result = minimize(fun=target_function_coordinates,
-                      x0=np.array([x0, y0, z0]),
+                      x0=(x0, y0, z0),
                       args=[coeffs, frequency])
     x, y, z = result.x
     z = abs(z)
     return x, y, z
+
+
+def minimization_velocities(points_list, coeffs, start_coords, start_velocity, frequency):
+    """
+    Функция минимизации для поиска скоростей
+    :param data: массив для целевой функции
+    :param start_coords: начальные координаты поиска событий
+    :param frequency: частота дискретизации
+    :return: x, y, z события
+
+    """
+    x0, y0, z0 = start_coords
+    points_count = len(points_list)
+    v0 = [start_velocity]*points_count
+
+    x0 = [x0, y0, z0]+v0
+
+    result = minimize(fun=target_function_velocities,
+                      x0=x0,
+                      args=[points_list, coeffs, frequency])
+    velocities = result.x[3:]
+    return velocities
--- SeisCore/tests/test1.py
+++ SeisCore/tests/test1.py
@@ -1,20 +1,2 @@
-import numpy as np
-
-def reproject_coords(points_coords, x_central=0, y_central=0):
-    """
-    Функция перевода координат в условную СК
-    :param points_coords: двухмерный массив numpy с координатами датчиков
-    :param x_central: координата x центральной точки
-    :param y_central: координата y центральной точки
-    :return: список экземпляров класса GRPPoint c пересчитанными координатами
-    """
-    # пересчет координат
-    result=np.empty(shape=(points_coords.shape[0],2),dtype=float)
-    for i in range(points_coords.shape[0]):
-      result[i,0]=points_coords[i,0] - x_central
-      result[i,1]=points_coords[i,0] - y_central
-    return result
-
-a=np.array([[0,0],[10,10],[0,10]])
-b=reproject_coords(a, x_central=10, y_central=10)
-print(b)
+a=[0,1,2,3,4,77,6,7,8,9]
+print(a.index(77))
