6f807ab07d22f8d6174f9169e788f375f54433d1
Michael <mihail.tchernov@yandex.ru>
2020-04-22 16:40:56 +0300

    version 0.3.17 Fix Filter module - sl_filter

--- SeisCore/Functions/Filter.py
+++ SeisCore/Functions/Filter.py
@@ -11,14 +11,9 @@ def band_pass_filter(signal, frequency, f_min, f_max):
     :param f_max: maximal filtering frequency
     :return: output 1-D array of signal
     """
-    # построение частотного ряда
     frequency_array = rfftfreq(n=signal.shape[0], d=1.0 / frequency)
-    # прямое преобразование Фурье
     f_signal = rfft(signal)
-    # зануление амплитуд, частоты которых меньше f_min
     f_signal[((frequency_array < f_min)+(frequency_array > f_max))] = 0
-    # зануление амплитуд, частоты которых больше f_max
-    # обратное преобразование Фурье (получится отфильтрованный сигнал)
     filtered_signal = irfft(f_signal)
     return filtered_signal
 
