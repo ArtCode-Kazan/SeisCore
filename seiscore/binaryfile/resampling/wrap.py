2158fae66afb22a0f7087a8b3d25567bec5915e7
Michael <mihail.tchernov@yandex.ru>
2021-05-11 17:11:14 +0300

    Fix bug with importing cython resample function

--- seiscore/binaryfile/resampling/wrap.py
+++ seiscore/binaryfile/resampling/wrap.py
@@ -1,6 +1,11 @@
+import os
+import sys
+
 import numpy as np
-from seiscore.binaryfile.resampling import resampling as resample_core
 
 
 def resampling(arr: np.ndarray, resample_parameter: int) -> np.ndarray:
+    so_file_path = os.path.dirname(__file__)
+    sys.path.insert(0, so_file_path)
+    from seiscore.binaryfile.resampling import resampling as resample_core
     return resample_core.resampling(arr, resample_parameter)
