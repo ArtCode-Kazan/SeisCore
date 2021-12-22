5405edc36a0ae43b74202e4af2e47bb60a6de228
Michael <mihail.tchernov@yandex.ru>
2021-12-22 13:03:32 +0300

    Переименован модуль с "медленной" функцией ресемплирования
    Упрощена функция ресемплирования

--- seiscore/binaryfile/resampling/prototype.py
+++ seiscore/binaryfile/resampling/prototype.py
@@ -5,11 +5,8 @@ def resampling(signal: np.ndarray, resample_parameter: int) -> np.ndarray:
     discrete_amount = signal.shape[0]
     resample_discrete_amount = discrete_amount // resample_parameter
     resample_signal = np.zeros(shape=resample_discrete_amount, dtype=np.int32)
+
     for i in range(resample_discrete_amount):
-        sum_val = 0
-        for j in range(resample_parameter):
-            k = i * resample_parameter + j
-            sum_val += signal[k]
-        value = sum_val // resample_parameter
-        resample_signal[i] = value
+        sum_val = sum(signal[i * resample_parameter: (i + 1) * resample_parameter])
+        resample_signal[i] = int(sum_val / resample_parameter)
     return resample_signal
