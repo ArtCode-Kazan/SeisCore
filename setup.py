3925345a48ed8e3008e279d5f08850d3175c2a62
Michael <mihail.tchernov@yandex.ru>
2021-12-22 14:42:30 +0300

    Изменена версия пакета

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup
 from setuptools import find_packages
 
+setup(
+    name='seiscore',
+    version='0.7.0',
+    description='Package for processing of microseismic data',
+    author='Michael Chernov',
+    author_email='mihail.tchernov@yandex.ru',
+    license='MIT',
 
-setup(name='seiscore',
-      version='0.6.0',
-      packages=find_packages(),
-      description='Package for processing of microseismic data',
-      author='Michael Chernov',
-      author_email='mikkoartic@gmail.com',
-      license='MIT',
-      include_package_data=True,
-      zip=False,
-      install_requires=[
-          'numpy==1.20.3',
-          'scipy==1.6.3',
-          'pywavelets==1.1.1',
-          'matplotlib==3.4.2'],
-      package_data={'seiscore': ['binaryfile/resampling/*.so']})
+    packages=find_packages(),
+    include_package_data=True,
+    zip=False,
+    install_requires=[
+        'numpy==1.20.3',
+        'scipy==1.6.3',
+        'pywavelets==1.1.1',
+        'matplotlib==3.4.2'
+    ],
+    package_data={'seiscore': ['binaryfile/resampling/*.so']}
+)
